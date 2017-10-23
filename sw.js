d7da4ad2987ad110e07b830944327310b15df0be
luoye <luoyefe@gmail.com>
2017-10-23 18:44:04 +0800

    update: sw微调

--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
-const CACHE_KEY = 'luoyefe-cache-v1';
-const CACHE_VERSION = '1.0.0';
+const CACHE_KEY = 'luoyefe';
+const CACHE_VERSION = '2.0.0';
 
 self.addEventListener('install', function(event) {
 	// 强制缓存
-	let forceCacheUrls = [
-		'/'
-	];
-	forceCacheUrls = forceCacheUrls.map(url => new Request(url, { credentials: 'include' }));
+	// let forceCacheUrls = [
+	// 	'/'
+	// ];
+	// forceCacheUrls = forceCacheUrls.map(url => new Request(url, { credentials: 'include' }));
 	event.waitUntil(
 		caches
 		.open(CACHE_KEY + '-v' + CACHE_VERSION)
-		.then(cache => cache.addAll(forceCacheUrls))
+		// .then(cache => cache.addAll(forceCacheUrls))
 	);
 });
 
@@ -18,9 +18,14 @@ self.addEventListener('fetch', function(event) {
 	const request = event.request;
 	const urlObj = new URL(event.request.url);
 
+	// console.log(urlObj);
+
+	// sw blog 不缓存
+	if (/sw\.js/.test(urlObj.pathname) || urlObj.pathname === '/blog/' || urlObj.pathname === '/blog') return fetch(request);
+
 	// 策略一 缓存优先（静态资源）
 	// 策略二 网络优先 （接口、HTML等）
-	if (/\.js|json|css|png|jpe?g|gif|webp|svg|ico|woff2?|eot|ttf|otf$/i.test(urlObj.pathname)) {
+	if (/\.js|css|png|jpe?g|gif|webp|svg|ico|woff2?|eot|ttf|otf$/i.test(urlObj.pathname)) {
 		event.respondWith(
 			caches
 			.match(request)
@@ -63,22 +68,22 @@ self.addEventListener('fetch', function(event) {
 	}
 });
 
-self.addEventListener('activate', function(event) {
-	// 删除缓存
-	const currentCache = CACHE_KEY + '-v' + CACHE_VERSION;
-	caches
-		.keys()
-		.then(function(cacheNames) {
-			return Promise.all(
-				cacheNames.map(function(cacheName) {
-					if (cacheName.indexOf(CACHE_KEY) == -1) {
-						return;
-					}
+// self.addEventListener('activate', function(event) {
+// 	// 删除缓存
+// 	const currentCache = CACHE_KEY + '-v' + CACHE_VERSION;
+// 	caches
+// 		.keys()
+// 		.then(function(cacheNames) {
+// 			return Promise.all(
+// 				cacheNames.map(function(cacheName) {
+// 					if (cacheName.indexOf(CACHE_KEY) == -1) {
+// 						return;
+// 					}
 
-					if (cacheName != currentCache) {
-						return caches.delete(cacheName);
-					}
-				})
-			);
-		});
-});
+// 					if (cacheName != currentCache) {
+// 						return caches.delete(cacheName);
+// 					}
+// 				})
+// 			);
+// 		});
+// });
